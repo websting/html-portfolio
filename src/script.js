10afe37c73730dda8c5c3aadd1d067b818f50dea
websting <herrera.antonio1@outlook.com>
2025-06-07 10:28:04 -0400

    first commit

---
+++ src/script.js
@@ -0,0 +1,29 @@
+// // sidebar menu
+const sideMenu = document.getElementById('sideMenu');
+
+const openMenu = () => {
+    sideMenu.style.right = '0'; 
+}
+
+const closeMenu = () => {
+    sideMenu.style.right = '-200px';
+}
+
+// submit to google-sheet script
+// code downloaded from GitHub
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw39qILECH0O-v5at43Lq-ygY9-a1rzSqlX_Qi731dnQtvK32kh9Q3lFmllllARSsIk/exec'
+            const form = document.forms['submit-to-google-sheet']
+            const msg = document.getElementById('msg')
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => {
+                    msg.innerHTML = 'Message Sent Successfully!'
+                    setTimeout(function(){
+                        msg.innerHTML = ''
+                    },5000)
+                    form.reset()
+                })
+                .catch(error => console.error('Error!', error.message))
+            })
